5701181aca9fe6e4c032c0944107ee6a116593f2
Nicolas Barron Sour <71356372+nbarsou@users.noreply.github.com>
2025-09-11 01:29:29 -0600

    fix: useSearchParams() should be wrapped in a suspense boundary at page /[locale]/auth/new-password & /[locale]/auth/new-verification

--- src/app/[locale]/auth/new-verification/page.tsx
+++ src/app/[locale]/auth/new-verification/page.tsx
@@ -1,7 +1,12 @@
 import { NewVerifcationForm } from "@/components/auth/new-verification-form";
+import { Suspense } from "react";
 
 const NewVerifcation = () => {
-  return <NewVerifcationForm />;
+  return (
+    <Suspense fallback={""}>
+      <NewVerifcationForm />;
+    </Suspense>
+  );
 };
 
 export default NewVerifcation;
